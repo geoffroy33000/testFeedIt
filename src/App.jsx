0f95c4317d43cde222cd70f0cc9bf35a3751948d
Geoffroy3 <geoffroy.pailhebelair@gmail.com>
2026-01-22 12:16:22 +0100

    Change the reducer to handle the previous and next elements in the carousel and make the modifications in the component.

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,21 @@ import Carrousel from './components/Carrousel/Carrousel.jsx';
 import List from './components/List/List.jsx';
 import { useData } from './hooks/useData.js';
 
+export const ALLOWED = [
+	'live_bullet',
+	'live_blitz',
+	'live_rapid',
+	'daily',
+	'tactics',
+];
+
 export default function App() {
 	const { rankByTimeControl, isError, isLoading } = useData();
 
-	const ALLOWED = [
-		'live_bullet',
-		'live_blitz',
-		'live_rapid',
-		'daily',
-		'tactics',
-	];
-
-	const filteredRank = rankByTimeControl.filter(([key]) => ALLOWED.includes(key));
-
 	if (isLoading) return <>Recherche des meilleurs joueurs...</>;
 	if (isError) return <>Erreur de chargement des données</>;
+	const filteredRank = rankByTimeControl.filter(([key]) => ALLOWED.includes(key));
+
 	return (
 		<main className='app'>
 			<h1>Classement</h1>
